22e3016a8b0cb198554e349d5199cd767a99962e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-27 05:20:46 +0000

    Deploy luanorlandi/spotifood to github.com/luanorlandi/spotifood.git:gh-pages

---
+++ precache-manifest.f515419ec672967ca6999fcec14d27e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2e54d6f759c3599ac5583d9022f57f",
+    "url": "/spotifood/index.html"
+  },
+  {
+    "revision": "04ed6f8b5018f8ccd6f3",
+    "url": "/spotifood/static/css/2.78ef715f.chunk.css"
+  },
+  {
+    "revision": "1b456766139dc738e865",
+    "url": "/spotifood/static/css/main.ce47f4c6.chunk.css"
+  },
+  {
+    "revision": "04ed6f8b5018f8ccd6f3",
+    "url": "/spotifood/static/js/2.18a9dd21.chunk.js"
+  },
+  {
+    "revision": "d86d8996d4d92f53321bbe8d87f09e97",
+    "url": "/spotifood/static/js/2.18a9dd21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b456766139dc738e865",
+    "url": "/spotifood/static/js/main.6b01e40c.chunk.js"
+  },
+  {
+    "revision": "277840213af827cc01b8",
+    "url": "/spotifood/static/js/runtime-main.cc4ac45f.js"
+  },
+  {
+    "revision": "f66bfb1e9f5751e23a4f0b7364bc3ca6",
+    "url": "/spotifood/static/media/search-solid.f66bfb1e.svg"
+  }
+]);
